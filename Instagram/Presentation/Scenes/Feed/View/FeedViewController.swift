54dec32f40fdaa9141dc559c91111ed72b44cd24
sanaz Bahmankhah <Sanaz.bahmankhah@tekever.com>
2022-12-13 12:09:38 +0100

    Setup collectionView.

--- Instagram/Presentation/Scenes/Feed/View/FeedViewController.swift
+++ Instagram/Presentation/Scenes/Feed/View/FeedViewController.swift
@@ -9,9 +9,36 @@ import Foundation
 import UIKit
 
 final class FeedViewController: UIViewController {
+    private let layout = UICollectionViewFlowLayout()
+    private var collectionView: UICollectionView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupCollectionView()
+        registerCell()
+    }
+    
+    func setupCollectionView() {
+        self.collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        guard let collectionView = self.collectionView else { return }
+        self.view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func registerCell() {
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "reuseIdentifier")
+    }
+}
+
+extension FeedViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseIdentifier", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
     }
 }
