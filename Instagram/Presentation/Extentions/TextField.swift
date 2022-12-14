f46f2c4be06afbdac18f06995ba58f892d651f6a
sanaz Bahmankhah <Sanaz.bahmankhah@tekever.com>
2022-12-14 10:22:43 +0100

    Added extentions.

---
+++ Instagram/Presentation/Extentions/TextField.swift
@@ -0,0 +1,30 @@
+//
+//  TextField.swift
+//  Instagram
+//
+//  Created by sanaz bahmankhah on 13/12/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    
+    func setPadding(left: CGFloat, right: CGFloat? = nil) {
+        setLeftPadding(left)
+        if let rightPadding = right {
+            setRightPadding(rightPadding)
+        }
+    }
+
+    private func setLeftPadding(_ padding: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    private func setRightPadding(_ padding: CGFloat) {
+         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.size.height))
+         self.rightView = paddingView
+         self.rightViewMode = .always
+     }
+ }
