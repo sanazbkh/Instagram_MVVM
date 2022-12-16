6294787a85d1cb974c7744c90b596c717da7418b
sanaz Bahmankhah <Sanaz.bahmankhah@tekever.com>
2022-12-16 10:45:12 +0100

    Created Registration class.

---
+++ Instagram/Presentation/Scenes/Login/View/RegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationViewController.swift
+//  Instagram
+//
+//  Created by sanaz bahmankhah on 13/12/2022.
+//
+
+import Foundation
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
